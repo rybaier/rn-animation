a07828535341e6b29ef33b0c98d001a47daff446
rybaier <rybaier@gmail.com>
2022-05-20 08:23:04 -0700

    forceSwipe

--- src/Deck.js
+++ src/Deck.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View, Animated, Text, StyleSheet, PanResponder, PanResponderCallbacks, Dimensions } from 'react-native'
+import { View, Animated, Text, StyleSheet, Dimensions, 
+    PanResponder, PanResponderCallbacks } from 'react-native'
 import { Card, Button, Image } from 'react-native-elements'
-const Deck = ({ data, renderCard }) => {
+
+const Deck = ({ data, renderCard, onSwipeRight, onSwipeLeft }) => {
     const SCREEN_WIDTH = Dimensions.get('window').width
     const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
-    
+    const SWIPEOUT_DURATION = 250
+
     let position = new Animated.ValueXY()
   
 
@@ -15,8 +18,10 @@ const Deck = ({ data, renderCard }) => {
         },
         onPanResponderRelease: (event, gesture) => {
             if (gesture.dx > SWIPE_THRESHOLD){
+                forseSwipe('right')
                 console.log('swipe right')
             } else if (gesture.dx < SWIPE_THRESHOLD) {
+                forseSwipe('left')
                 console.log('swipe left')
             } else {
                 resetPosition()
@@ -25,13 +30,25 @@ const Deck = ({ data, renderCard }) => {
         
     })
     
+    const forseSwipe = (direction) => {
+        const x = direction ==='right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+        Animated.timing(position, {toValue: { x: x, y: 0 }, 
+        duration: SWIPEOUT_DURATION }).start(() => onSwipeComplete(direction))
+    }
+
+    const onSwipeComplete = (direction) => {
+        // const { onSwipeLeft, onSwipeRight } =
+        // direction === 'right' ? onSwipeRight() ? onSwipeLeft() 
+    }
+
     const resetPosition = () => {
         Animated.spring(position, {toValue: {x: 0, y:0 }}).start()
     }
 
+
     const getCardStyle = () => {
         const rotate = position.x.interpolate({
-            inputRange: [-500, 0, 500],
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
             outputRange: ['120deg', '0deg', '120deg']
         })
 
