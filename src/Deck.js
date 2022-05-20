863a0c3e432a83d7a65eacd4da2c7518742956d1
rybaier <rybaier@gmail.com>
2022-05-20 07:58:20 -0700

    spring on release

--- src/Deck.js
+++ src/Deck.js
@@ -1,31 +1,37 @@
 import React from 'react'
-import { View, Animated, Text, StyleSheet, Dimensions, 
-    PanResponder, PanResponderCallbacks } from 'react-native'
+import { View, Animated, Text, StyleSheet, PanResponder, PanResponderCallbacks, Dimensions } from 'react-native'
 import { Card, Button, Image } from 'react-native-elements'
-
 const Deck = ({ data, renderCard }) => {
     const SCREEN_WIDTH = Dimensions.get('window').width
-
+    const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
+    
     let position = new Animated.ValueXY()
+  
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderMove : (event, gesture ) =>{ 
             position.setValue({ x: gesture.dx, y: gesture.dy })
         },
-        onPanResponderRelease: () => {
-            resetPosition()
+        onPanResponderRelease: (event, gesture) => {
+            if (gesture.dx > SWIPE_THRESHOLD){
+                console.log('swipe right')
+            } else if (gesture.dx < SWIPE_THRESHOLD) {
+                console.log('swipe left')
+            } else {
+                resetPosition()
+            }
         }
         
     })
     
     const resetPosition = () => {
-        
+        Animated.spring(position, {toValue: {x: 0, y:0 }}).start()
     }
 
     const getCardStyle = () => {
         const rotate = position.x.interpolate({
-            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            inputRange: [-500, 0, 500],
             outputRange: ['120deg', '0deg', '120deg']
         })
 
