b42344fd73b1a6bed8f2e381912bdb8463825845
rybaier <rybaier@gmail.com>
2022-10-29 15:48:54 -0400

    user functions created

--- one-time-password/functions/create_user.js
+++ one-time-password/functions/create_user.js
@@ -1,3 +1,17 @@
+const admin = require('firebase-admin');
+
 module.exports= function(req, res) {
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({error: 'Bad Input'});
+  }
+  // Format the phone number to remove the dashes and parens
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  // Create a user account using that phone number//async request
+  admin.auth().createUser({ uid: phone }) 
+      .then(user => res.send(user))
+      .catch(err => res.status(422).send({ error: err }))
+  // Respond to the user request, saying the account was made
+
 };
--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -3,9 +3,10 @@ const admin = require("firebase-admin");
 
 const functions = require("firebase-functions");
 const createUser = require("./create_user");
+const serviceAccount = require("./service_account.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 exports.createUser = functions.https.onRequest(createUser);
