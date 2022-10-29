2cfdc10d2ce3c63512a506e7392c17e700658eb6
rybaier <rybaier@gmail.com>
2022-10-29 15:01:06 -0400

    git ignore test

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,22 +1,11 @@
 /* eslint-disable max-len */
+const admin = require("firebase-admin");
+
 const functions = require("firebase-functions");
 const createUser = require("./create_user");
 
-const admin = require("firebase-admin");
-admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  // eslint-disable-next-line object-curly-spacing
-  // functions.logger.info("Hello logs!", { structuredData: true }); <-- this is causes permissions error
-  //                even with allUsers allowed on
-  response.send("Hello from Firebase!");
-});
-
-exports.goodbye = functions.https.onRequest(( request, response) => {
-  response.send("Goodbye");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
 });
 
 exports.createUser = functions.https.onRequest(createUser);
