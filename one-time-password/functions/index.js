c1ffc9c0d3fe90ad9f384e845e4193a036099215
rybaier <rybaier@gmail.com>
2022-11-01 15:30:52 -0400

    twilio setup

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -4,9 +4,11 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const createUser = require("./create_user");
 const serviceAccount = require("./service_account.json");
+const requestOneTimePassword = require('./request_one_time_password')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 exports.createUser = functions.https.onRequest(createUser);
+exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword)
---
+++ one-time-password/functions/twilio.js
@@ -0,0 +1,5 @@
+const twilio = require('twilio')
+
+const accountSid = process.env.ACCOUNTSID
+const authToken = process.env.AUTHTOKEN
+
