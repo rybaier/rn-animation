aa94166af9b1aca13294e797cf5bc57aab0b95cb
rybaier <rybaier@gmail.com>
2022-11-03 15:35:46 -0400

    repo transfer

--- one-time-password/functions/twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require('twilio')
-// accountSID and AUTHTOKEN in .env remove before git push 
-const accountSid = process.env.ACCOUNTSID
-const authToken = process.env.AUTHTOKEN
-
-module.exports = new twilio.Twilio(accountSid, authToken);
